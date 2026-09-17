a5828b79c8c4dd79bfe072bf9766747154bfb166
vishwas_maurya <vishwas.maurya@shilshatech.com>
2026-09-17 07:11:27 +0000

    conversion of data types

---
+++ 01_basics/conversion.js
@@ -0,0 +1,29 @@
+// let age ="222"
+
+// console.log(typeof age)
+
+// let valueInNumber =Number(age)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let score=null
+
+// console.log(typeof null)
+
+// let num=Number(score)
+
+// console.log(typeof num)
+// console.log(num)
+
+// let value="0"
+
+// let booleanValue=Boolean(value)
+// console.log(typeof booleanValue)
+// console.log(booleanValue)
+
+let value=5454
+
+let stringValue=String(value)
+console.log(typeof stringValue)
+console.log(stringValue)
