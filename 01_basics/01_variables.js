ecdd03aef167d3c5b9525dd26d84a1289d9468ec
vishwas_maurya <vishwas.maurya@shilshatech.com>
2026-09-17 06:32:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+// const accountId=232838;
+// var name="jarina";
+// let email="jarina@gmail.com";
+
+// city="jaipur";
+
+// accountId=32323
+
+// console.table([accountId,name,email,city]);
+
+function test(){
+    if(true){
+        let x=10;
+    }
+    console.log(x);
+}
+test()
---
+++ 01_basics/02_dataytypes.js
@@ -0,0 +1,11 @@
+//number
+//bigint
+//string -> ""
+//boolean
+//null
+//undifined
+//symbol
+//object
+
+console.log(typeof "shma")
+console.log(typeof null)
